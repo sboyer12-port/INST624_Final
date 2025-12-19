04be6e95c579551eae677faeb7add21528fb9b05
Sebastian Boyer <sboyer12@umd.edu>
2025-12-18 20:15:45 -0500

    Created my first function. Created my instances as well for the stock data I am collecting.

--- Analyzer.py
+++ Analyzer.py
@@ -2,3 +2,10 @@
 
 import requests # already downloaded through pip
 import pandas as pd
+
+class StockAnalyzer:
+    def __init__(self, symbol, apikey): # these are the variable that I am going to mainly focusing on. Most important information of the stock
+        self.symbol = symbol # creating my methods
+        self.apikey = apikey
+        self.data = None # null value for data
+        
