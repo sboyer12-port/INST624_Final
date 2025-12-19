0bb3b95a7e5708189d411a6c4ea40ad163550167
Sebastian Boyer <sboyer12@umd.edu>
2025-12-19 14:32:19 -0500

    I understand that I might lose points for not having more updates in Git. However I couldn't find the appropriate time to update the git until it was all completed because everything didn't work until the entire program was finished. This is my working model.

--- Analyzer.py
+++ Analyzer.py
@@ -11,8 +11,35 @@ class StockAnalyzer:
         
     def pulldata(self):
         url = "https://www.alphavantage.co/query" # this is the url we need to run our query. This is what will be ran through the terminal
-        paramaters = {
+        params = {
             "function": "TIME_SERIES_DAILY", # this is the function that we are returning from alpha vantage
             "symbol": self.symbol,
             "apikey": self.apikey
+        }
+
+        response = requests.get(url, params=params) # arguably the most important line in the program. This is sending our GET request to the API
+        json_data = response.json() # this turns the response into a json response
+
+        self.data = json_data["Time Series (Daily)"] # this is very important because it takes the daily stock price (I struggled with this too because I had to search to find the correct thing to write specifically for this variable)
+
+
+        # below I am creating a method coverts the API data into a pandas DataFrame
+    def dataframe(self):
+        df = pd.DataFrame.from_dict(self.data, orient="index") # orient="index" is the way I found to appropriatly sort the data
+        df = df.astype(float)
+        df.index = pd.to_datetime(df.index)
+        df.sort_index(inplace=True)
+        return df 
+
+        # for this below I also struggled for a while with the variable names
+    def analyze(self, df):
+        avg_close = df["4. close"].mean()
+        max_close = df["4. close"].max()
+        min_close = df["4. close"].min()
+
+            # returns the program
+        return {
+        "average_close": avg_close,
+        "max_close": max_close,
+        "min_close": min_close
         }
--- StockAnalyzer.py
+++ StockAnalyzer.py
@@ -1,4 +1,35 @@
+# Sebastian Boyer Final Project INST624
 # Stock Analyzer
 
 import sys
 import matplotlib.pyplot as plt
+from Analyzer import StockAnalyzer
+
+API_KEY = "ASW4FSIWUW4Z009O" # this is my api key
+
+if len(sys.argv) < 2:
+    print("You forgot to add a stock symbol. Make sure to do that and try again.") # this code is just in case there isn't a stock symbol added.
+    sys.exit(1)
+
+symbol = sys.argv[1] # wrote this to take the first argument given in the terminal which will be the stock name, and then store it in "symbol"
+
+analyzer = StockAnalyzer(symbol, API_KEY) # creating an object from the stock analyzer class that shows what stock and api key to use
+analyzer.pulldata() # calling the pulldata method to make the request
+df = analyzer.dataframe() # creating a pandas dataframe
+stats = analyzer.analyze(df) # this method is to create/compile the statistics and then eventually return them as a dictionary
+
+
+# so below I had to tweak several times. I didn't realize at first that I had to use specific variable names for the stats class and that was broken for a while
+print(f"\nStock Analysis for {symbol}")
+print(f"Average Close Price: ${stats['average_close']:2f}")
+print(f"Highest Close Price: ${stats['max_close']:2f}")
+print(f"Lowest Close Price: ${stats['min_close']:2f}")
+
+# below is the creation of our graoh through myplotlib
+plt.figure()
+plt.plot(df.index, df["4. close"])
+plt.title(f"{symbol} Closing Prices Over Time")
+plt.xlabel("Date")
+plt.ylabel("Price US")
+plt.tight_layout()
+plt.show()
